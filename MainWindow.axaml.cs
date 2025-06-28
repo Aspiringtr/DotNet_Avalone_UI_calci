f112787cd61c11b12519cd117cb831abc33c4f59
Chithambaranathan V <chithambaranathanv6@gmail.com>
2025-06-28 23:48:58 +0530

    first commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace calculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# DotNet_Avalone_UI_calci" 
