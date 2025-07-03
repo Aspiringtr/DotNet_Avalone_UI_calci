cc54d034d312b61386a328dad8f6e66824faf5fb
Chithambaranathan V <aspirin.gtr@gmail.com>
2025-07-04 00:40:08 +0530

    completed

--- obj/Debug/net8.0/calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e78f3590fd8874782022838b27ab786ef2be071")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c3bc20651e92012666f37a6f5402ffe5d9bfba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
